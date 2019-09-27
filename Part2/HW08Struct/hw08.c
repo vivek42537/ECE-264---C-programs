1617906394c6af3c875a1c3b24ef9be58552333a
Vivek Shashank Khanolkar <vkhanolk@ecegrid-thin1.ecn.purdue.edu>
2019-09-27 18:49:36 -0400

    cash

--- Part2/HW08Struct/hw08.c
+++ Part2/HW08Struct/hw08.c
@@ -7,7 +7,6 @@
 #include <stdbool.h>
 #include "hw08.h"
 
-
 #ifdef TEST_COUNTVECTOR
 int countVector(char * filename)
 {
@@ -20,6 +19,24 @@ int countVector(char * filename)
   //
   // For the mode of fopen, you may use "r" without b
   //
+  FILE * fur = fopen(filename, "r");
+  int c=0; // c 
+  Vector v;
+  if (fur == NULL)
+  {
+   return -1;
+  }
+  else
+  { 
+   while (((fread(&v, sizeof(Vector), 1, fur)) == 1))
+   {
+     c++;
+   }
+  }
+   
+  fclose(fur);  
+
+  return c;
 }
 #endif
 
@@ -34,7 +51,25 @@ bool readVector(char* filename, Vector * vecArr, int size)
   // few or too many) return false
   // 
   // if everything is fine, fclose and return true
+  FILE * furr = fopen(filename, "r");
 
+  if (furr == NULL)
+  {
+   return false;
+  }
+  else
+  {
+   if (((fread(vecArr, sizeof(Vector), size, furr)) == size))
+   {
+     fclose(furr);
+     return true;
+   }  
+   else
+   {
+    fclose(furr);
+    return false;   
+   }
+  }
 }
 #endif
 
@@ -59,6 +94,46 @@ int compareVector(const void *p1, const void *p2)
   // If the first vector's z is greater than the second vector's z
   // return 1
   // If the two vectors' x, y, z are the same (pairwise), return 0
+  const Vector * ptr1 = (const Vector *) p1;
+  const Vector * ptr2 = (const Vector *) p2;
+  Vector v1 = * ptr1;
+  Vector v2 = * ptr2;
+
+  if (v1.x < v2.x)
+  {
+   return -1;
+  }
+  else if (v1.x > v2.x)
+  {
+   return 1;
+  }
+  else if (v1.x == v2.x)
+  {
+   if (v1.y < v2.y)
+   {
+    return -1;
+   }
+   else if (v1.y > v2.y)
+   {
+    return 1;
+   }
+   else if (v1.y == v2.y)
+   {
+    if (v1.z < v2.z)
+    {
+     return -1;
+    }
+    else if (v1.z > v2.z)
+    {
+     return 1;
+    }
+    else if (v1.z == v2.z)
+    {
+     return 0;
+    }
+   }
+  }
+ return 0;
 }
 #endif
 
@@ -71,6 +146,23 @@ bool writeVector(char* filename, Vector * vecArr, int size)
   // if not all are written, fclose and return false
   // 
   // fclose and return true
+  FILE * fum = fopen(filename, "w");
+  int w; // how many written
+ 
+  if (fum == NULL)
+  {
+   return false;
+  }
+  w = fwrite(vecArr, sizeof(Vector), size, fum);
+ 
+  if (w != size)
+  {
+   fclose(fum);
+   return false;
+  }  
+   
+  fclose(fum);
+  return true;
 }
 #endif
 
--- Part2/HW08Struct/main.c
+++ Part2/HW08Struct/main.c
@@ -11,36 +11,70 @@
 #ifdef TEST_MAIN
 int main(int argc, char * * argv)
 {
-  // argv[1]: name of input file (binary)
-  // argv[2]: name of output file (binary)
+	// argv[1]: name of input file (binary)
+	// argv[2]: name of output file (binary)
 
-  // check whether there are three arguments.
-  // If not, return EXIT_FAILURE. DO NOT print anything
+	// check whether there are three arguments.
+	// If not, return EXIT_FAILURE. DO NOT print anything
 
-  // use argv[1] as the input to countVector, save the result
+	// use argv[1] as the input to countVector, save the result
 
-  // if the number of vector is 0 or negative, return EXIT_FAILURE
+	// if the number of vector is 0 or negative, return EXIT_FAILURE
 
-  // otherwise, allocate memory for an array of vectors
+	// otherwise, allocate memory for an array of vectors
 
-  // read the vectors from the file whose name is argv[1]. save the
-  // results in the allocated array
-  // if reading fails, release memory and return EXIT_FAILURE
+	// read the vectors from the file whose name is argv[1]. save the
+	// results in the allocated array
+	// if reading fails, release memory and return EXIT_FAILURE
+
+	if (argc < 3)
+	{
+		return EXIT_FAILURE;
+	}
+
+	int numElem = countVector(argv[1]);
+	if(numElem <= 0)
+	{
+		return EXIT_FAILURE;
+	}
+
+	Vector * vecArr;
+	vecArr = malloc(numElem * sizeof(Vector)); 
+       
+        if (vecArr == NULL)
+        {
+         free(vecArr);
+         return EXIT_FAILURE;
+        }  
+ 
+	bool r = readVector(argv[1], vecArr, numElem);
+	if (r == false) // read fail
+	{
+		return EXIT_FAILURE;  
+	}
 
 #ifdef DEBUG
-  printVector(vecArr, numElem);
+	printVector(vecArr, numElem);
 #endif  
 
-  
+ qsort(vecArr, numElem, sizeof(Vector), compareVector);
+
 #ifdef DEBUG
-  printf("\n");
-  printVector(vecArr, numElem);
+	printf("\n");
+	printVector(vecArr, numElem);
 #endif  
 
-  // write the sorted array to the file whose name is argv[2]
-  // if writing fails, release memory and return EXIT_FAILURE
-  
+	// write the sorted array to the file whose name is argv[2]
+	// if writing fails, release memory and return EXIT_FAILURE
+	bool v = writeVector(argv[2], vecArr, numElem);
+	if(v == false)
+	{
+		free(vecArr);
+		return EXIT_FAILURE;
+	}  
 
-  // releave memory, return EXIT_SUCCESS
+	// releave memory, return EXIT_SUCCESS
+	free(vecArr);
+	return EXIT_SUCCESS;
 }
 #endif
