86ccf7f0c45af06712e447e448cbfd186f561f46
Vivek Shashank Khanolkar <vkhanolk@ecegrid-thin1.ecn.purdue.edu>
2019-10-11 01:17:00 -0400

    oneam

--- Part2/HW10Shuffle1/shuffle.c
+++ Part2/HW10Shuffle1/shuffle.c
@@ -31,6 +31,14 @@ static void printDeck(CardDeck deck)
 // cards from the bottom of the original deck.
 void divide(CardDeck origDeck, CardDeck * leftDeck, CardDeck * rightDeck)
 {
+ for(int j=0; j < (origDeck.size - 1);j++)
+ {
+  strncpy(leftDeck[j].cards, origDeck.cards, j + 1);
+  strncpy(rightDeck[j].cards, origDeck.cards + (j + 1), origDeck.size - j - 1);
+
+  leftDeck[j].size = j + 1;
+  rightDeck[j].size = origDeck.size - j - 1;
+ }
 }
 #endif
 
@@ -76,9 +84,83 @@ void divide(CardDeck origDeck, CardDeck * leftDeck, CardDeck * rightDeck)
 // function, please keep it inside #ifdef TEST_INTERLEAVE and #endif
 // so that the function can be removed for grading other parts of the
 // program.
-void interleave(CardDeck leftDeck, CardDeck rightDeck)
+void help(CardDeck nDeck, CardDeck leftDeck, CardDeck rightDeck)
 {
+ if(leftDeck.size == 0)
+ {
+  for(int k = 0; k < rightDeck.size; k++)
+  {
+   memcpy(&nDeck.cards[nDeck.size], &rightDeck.cards[k], 1);
+   nDeck.size = nDeck.size + 1;
+  }
+  printDeck(nDeck);
+  return;
+ }
+ 
+ if(rightDeck.size == 0)
+ {
+  for(int y = 0; y < leftDeck.size; y++)
+  {
+   memcpy(&nDeck.cards[nDeck.size], &leftDeck.cards[y], 1);
+   nDeck.size = nDeck.size + 1;
+  }
+  printDeck(nDeck);
+  return;
+ }
+
+ CardDeck nleft;
+ nleft.size = 0;
+ 
+ CardDeck nDeckcopy;
+ nDeckcopy.size = nDeck.size;
+ 
+ for(int z = 0; z < nDeck.size; z++)
+ {
+  memcpy(&nDeckcopy.cards[z], &nDeck.cards[z], 1);
+ }
+ memcpy(&nDeckcopy.cards[nDeckcopy.size], &leftDeck.cards[0], 1);
+ nDeckcopy.size += 1;
+
+ for(int m = 1; m < leftDeck.size; m++)
+ {
+  memcpy(&nleft.cards[m - 1], &leftDeck.cards[m], 1);
+  nleft.size += 1;
+ }
+ 
+ help(nDeckcopy, nleft, rightDeck);
+ 
+ CardDeck nright;
+ nright.size = 0;
+
+ CardDeck nDeckcopyB;
+ nDeckcopyB.size = nDeck.size;
+ 
+ for(int z = 0; z < nDeck.size; z++)
+ {
+  memcpy(&nDeckcopyB.cards[z], &nDeck.cards[z], 1);
+ }
+ memcpy(&nDeckcopyB.cards[nDeckcopyB.size], &rightDeck.cards[0], 1);
+ nDeckcopyB.size += 1;
+
+ for(int m = 1; m < rightDeck.size; m++)
+ {
+  memcpy(&nright.cards[m - 1], &rightDeck.cards[m], 1);
+   nright.size += 1;
+ }
+
+ help(nDeckcopyB, leftDeck, nright);
+
 }
+ void interleave(CardDeck leftDeck, CardDeck rightDeck)
+ {
+  CardDeck nDeck;
+  nDeck.size = 0;
+  help(nDeck, leftDeck, rightDeck);
+ }
+
+
+
+
 #endif
 
 #ifdef TEST_SHUFFLE
@@ -97,5 +179,36 @@ void interleave(CardDeck leftDeck, CardDeck rightDeck)
 //
 void shuffle(CardDeck origDeck)
 {
+ int numlr; 
+ numlr = origDeck.size - 1;
+ 
+ if(numlr == 0)
+ {
+  return;
+ }
+ 
+ CardDeck * leftDeck = NULL;
+ CardDeck * rightDeck = NULL;
+ 
+ leftDeck = malloc(numlr * sizeof(CardDeck));
+ rightDeck = malloc(numlr * sizeof(CardDeck));
+
+ divide(origDeck, leftDeck, rightDeck);
+ 
+ for(int i = 0; i < numlr; i++)
+ {
+  interleave(leftDeck[i], rightDeck[i]);
+ }
+ 
+ free(leftDeck);
+ free(rightDeck);
+ 
 }
 #endif
+
+
+
+
+
+
+
