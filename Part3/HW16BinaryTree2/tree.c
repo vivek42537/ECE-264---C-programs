afe1cbecc6d5b6708f5fa12d4c8609a7e7d86b1e
Vivek Shashank Khanolkar <vkhanolk@ecegrid-thin1.ecn.purdue.edu>
2019-11-18 15:33:12 -0500

    rapidamente

--- Part3/HW16BinaryTree2/tree.c
+++ Part3/HW16BinaryTree2/tree.c
@@ -45,13 +45,85 @@ void freeTree(Tree * tr)
 // *** You MUST modify the follow function
 // ***
 #ifdef TEST_BUILDTREE
+TreeNode * newNode(int k)
+{
+  TreeNode * node = (TreeNode*)(malloc(sizeof(TreeNode)));
+  node->value = k;
+  node->left = node->right = NULL;
+ 
+  return node;
+}
+
+int search(int * arg, int start, int end, int val)
+{
+ int i = 0;
+    for(i = start; i <= end; i++)
+    {
+          if(arg[i] == val)
+                  break;
+    }
+    return i;
+}
+
+ 
+
+TreeNode * build(int * inArray, int * postArray, int start, int end, int *ind)
+{
+  if(start > end)
+  	return NULL;
+  
+  TreeNode * node = newNode(postArray[*ind]);
+  (*ind)--;
+
+  if(start == end)
+	return node;
+  
+  
+  int iind = search(inArray, start, end, node->value);
+  node->right = build(inArray, postArray, iind + 1, end, ind);
+  node->left = build(inArray, postArray, start,  iind-1, ind);
+
+  return node;
+}
+
 Tree * buildTree(int * inArray, int * postArray, int size)
 {
+ int ind = size - 1;
+ Tree * tree = (Tree*)malloc(sizeof(Tree));
+ tree -> root = build(inArray, postArray, 0, size - 1, &ind);
+ return tree;
 }
+
 #endif
 
 #ifdef TEST_PRINTPATH
+int myPath(TreeNode * nood, int key)
+{
+  if(nood == NULL)
+        return 0;
+  if(nood->value == key)
+  {
+   printf("%d\n", nood->value);
+   return 1;
+  }
+
+  if(myPath(nood->left,key) || myPath(nood->right, key))
+  {
+   printf("%d\n", nood->value);
+   return 1;
+  }
+  else
+  {
+	return 0;
+  }
+}
+
 void printPath(Tree * tr, int val)
 {
+ myPath(tr -> root, val);
 }
 #endif
+
+
+
+
